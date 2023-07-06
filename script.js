a0d729aa8fccf87bdbbcff1ea01bbc5dbdd384d0
matt0219 <robotmatt05@gmail.com>
2023-07-06 10:05:27 -0400

    Now you can have multiple submissions!

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-document.getElementById("calorieForm").addEventListener("submit", function(event){
+document.getElementById("calorieForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
     let checkboxes = document.querySelectorAll('input[name="food"]:checked');
+    let totalCalories = parseInt(document.getElementById("totalCalories").textContent);
 
-    let totalCalories = 0;
-    for(var i = 0; i < checkboxes.length; i++) {
-        let foodInfo = checkboxes[i].value.split("|");
-        let calories = parseInt(foodInfo[1]);
-        totalCalories += calories;
+    for (var i = 0; i < checkboxes.length; i++) {
+      let foodInfo = checkboxes[i].value.split("|");
+      let calories = parseInt(foodInfo[1]);
+      totalCalories += calories;
     }
 
     document.getElementById("totalCalories").textContent = totalCalories;
 
-})
+    document.getElementById("calorieForm").reset();
+});
+
+
+
+
+
